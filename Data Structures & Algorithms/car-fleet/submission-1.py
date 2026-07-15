02bc32960ca0367a02e3c29c40ffe4a4d1142050
Mahesh170505 <maheshganesh@umass.edu>
2026-07-15 08:20:55 -0400

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        result = sorted(list(zip(position, speed)), reverse = True)
+        stack = []
+
+        for i in range(len(result)):
+            tta = (target - result[i][0]) / result[i][1]
+            if not stack or tta > stack[-1]:
+                stack.append(tta)
+        return len(stack)
+            
